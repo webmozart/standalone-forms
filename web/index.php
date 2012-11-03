5513bec95f65dccf5c2af1285ede263dc98e929e
Bernhard Schussek <bschussek@gmail.com>
2012-11-03 18:04:23 +0100

    Moved setup code to separate PHP file

--- web/index.php
+++ web/index.php
@@ -1,48 +1,11 @@
 <?php
 
-use Symfony\Component\Validator\Validation;
-use Symfony\Bridge\Twig\Form\TwigRendererEngine;
 use Symfony\Component\Validator\Constraints\MinLength;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Form\Forms;
-use Symfony\Component\Form\Extension\Validator\ValidatorExtension;
-use Symfony\Component\Form\Extension\Csrf\CsrfExtension;
-use Symfony\Component\Form\Extension\Csrf\CsrfProvider\DefaultCsrfProvider;
-use Symfony\Component\Translation\Translator;
-use Symfony\Component\Translation\Loader\XliffFileLoader;
-use Symfony\Bridge\Twig\Extension\TranslationExtension;
-use Symfony\Bridge\Twig\Extension\FormExtension;
-use Symfony\Bridge\Twig\Form\TwigRenderer;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-// Overwrite this with your own secret
-$csrfSecret = 'c2ioeEU1n48QF2WsHGWd2HmiuUUT6dxr';
-
-// Set up requirements - hopefully we can facilitate this more in 2.2
-$csrfProvider = new DefaultCsrfProvider($csrfSecret);
-$validator = Validation::createValidator();
-$translator = new Translator('en');
-$translator->addLoader('xlf', new XliffFileLoader());
-$translator->addResource('xlf', realpath(__DIR__ . '/../vendor/symfony/form/Symfony/Component/Form/Resources/translations/validators.en.xlf'), 'en', 'validators');
-$translator->addResource('xlf', realpath(__DIR__ . '/../vendor/symfony/validator/Symfony/Component/Validator/Resources/translations/validators.en.xlf'), 'en', 'validators');
-$loader = new Twig_Loader_Filesystem(array(
-    realpath(__DIR__ . '/../views'),
-    realpath(__DIR__ . '/../vendor/symfony/twig-bridge/Symfony/Bridge/Twig/Resources/views/Form'),
-));
-$twigFormEngine = new TwigRendererEngine(array('form_div_layout.html.twig'));
-$twig = new Twig_Environment($loader, array(
-    'cache' => realpath(__DIR__ . '/../cache'),
-));
-$twig->addExtension(new TranslationExtension($translator));
-$twig->addExtension(new FormExtension(new TwigRenderer($twigFormEngine, $csrfProvider)));
-$twigFormEngine->setEnvironment($twig);
-
-// Set up the form factory with all desired extensions
-$formFactory = Forms::createFormFactoryBuilder()
-    ->addExtension(new CsrfExtension($csrfProvider))
-    ->addExtension(new ValidatorExtension($validator))
-    ->getFormFactory();
+require __DIR__ . '/../src/setup.php';
 
 // Create our first form!
 $form = $formFactory->createBuilder()
